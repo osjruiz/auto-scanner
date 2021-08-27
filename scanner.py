0c272989c71d0d14c6eed59b68b7da0e9b68d36d
Jose Ruiz <jose.ruiz@alliantum.com>
2021-08-27 11:32:19 +0200

    fix: correctly pass port list to nmap

--- scanner.py
+++ scanner.py
@@ -1,23 +1,30 @@
 import xml.etree.ElementTree as ET
+import shlex
 import subprocess
 
 import click
 import nmap
 
 
+def splitter(cmd):
+    return shlex.split(cmd)
+
+
 def pinger(target):
-    cmd = ['ping', '-c', '1', '-W', '2', target]
-    if subprocess.call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0:
-        return True
-    else:
-        return False
+    return not bool(
+        subprocess.call(
+            splitter(f'ping -c 1 -W 2 {target}'),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    )
 
 
 def report_parser(target):
     host_ports = {}
     port_list = []
     try:
-        tree = ET.parse(f"{target}-scan.xml")
+        tree = ET.parse(f'{target}-scan.xml')
         root = tree.getroot()
 
         for host in root.findall('host'):
@@ -41,37 +48,46 @@ def masscan_scanner(target, interface, ports):
     if pinger(target) is False:
         print("Target doesn't seem to be reachable. Scan may fail.")
 
-    rate = "1000"
+    rate = '1000'
 
-    print(f"[+] Starting masscan for host: {target}")
-    scan = subprocess.Popen(['masscan', target, '--ports', ports, '--max-rate', rate, '-oX', f"{target}-scan.xml"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f'[+] Starting masscan for host: {target}')
+    scan = subprocess.Popen(
+        splitter(f'masscan {target} --ports {ports} --max-rate {rate} -oX {target}-scan.xml'),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
     scan.communicate()
 
-    return(report_parser(target))
 
-
-def nmap_scanner(masscan_result):
-    nmap_args = "-Pn -sS -sC -sV -T4 -p"
+def nmap_scanner(nmap_target):
+    ports = []
 
     nm = nmap.PortScanner()
-    for result in masscan_result:
-        for host, ports in result.iteritems():
-            args = nmap_args + ','.join(map(str, ports))
-            print(args)
-            print(f"[+] nmap scan for {host}")
-            nm.scan(host, arguments=args)
+    for result in nmap_target:
+        for port in nmap_target[result]:
+            ports.append(port)
+    print(f'[+] nmap scan for {result}')
+    nm.scan(
+        result,
+        arguments=f"-sS -sC -sV -T4 -p{','.join(map(str, ports))} -oN {result}-scan.txt",
+    )
 
-    print(nm.scaninfo())
+    return nm.csv()
 
 
 @click.command()
-@click.option("--target", default="", help="Target's IP address")
-@click.option("--interface", default="", help="Source interface for the scan")
-@click.option("--ports", default="0-65535", help="Port range to scan. Defaults to 0-65535 (all ports)")
+@click.option('--target', default='', help="Target's IP address")
+@click.option('--interface', default='', help='Source interface for the scan')
+@click.option(
+    '--ports',
+    default='0-65535',
+    help='Port range to scan. Defaults to 0-65535 (all ports)',
+)
 def main(target, interface, ports):
-    masscan_result = masscan_scanner(target, interface, ports)
-    nmap_scanner(masscan_result)
+    masscan_scanner(target, interface, ports)
+    nmap_target = report_parser(target)
+    nmap_scanner(nmap_target)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
